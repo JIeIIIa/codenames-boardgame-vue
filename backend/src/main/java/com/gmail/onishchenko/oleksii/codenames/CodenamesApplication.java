1b375fa40df9896e53a76ce7b76352d58fe665dd
JIeIIIa <stealth@bigmir.net>
2019-02-02 00:19:54 +0200

    Add backend module

---
+++ backend/src/main/java/com/gmail/onishchenko/oleksii/codenames/CodenamesApplication.java
@@ -0,0 +1,14 @@
+package com.gmail.onishchenko.oleksii.codenames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodenamesApplication.class, args);
+    }
+
+}
+
