920e0296cb8d06a74c648dc3bc83489b25e93be4
JIeIIIa <stealth@bigmir.net>
2019-02-02 18:35:23 +0200

    Add maven dependencies: log4j2, JUnit2, maven-compile-plugin, jacoco, coveralls plugin. Add Travis CI configuration

--- backend/src/test/java/com/gmail/onishchenko/oleksii/codenames/CodenamesApplicationTests.java
+++ backend/src/test/java/com/gmail/onishchenko/oleksii/codenames/CodenamesApplicationTests.java
@@ -1,16 +1,16 @@
 package com.gmail.onishchenko.oleksii.codenames;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CodenamesApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CodenamesApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
